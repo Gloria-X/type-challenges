396b0bc5de82050bbdeb2562aa90823197bd8e94
Gloria_x <656369946@qq.com>
2023-07-17 09:30:42 +0800

    easy

---
+++ type-challenges/easy12_unshift/template.ts
@@ -0,0 +1,2 @@
+// type Unshift<T, U> = any;
+type Unshift<T extends any[], U> = [U, ...T];
---
+++ type-challenges/easy13_parameters/template.ts
@@ -0,0 +1,10 @@
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...arg: infer P
+) => unknown
+  ? P
+  : never;
+
+/*
+infer -> 推导泛型参数
+因为 (a: string, b: number)=> void extends (a: infer P,b: infer P) => void，所以(a: string, b: number)=> void  是   (a: infer P,b: infer P) => void 子类型
+*/
