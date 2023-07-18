c926372dd7fa98aa9db93010cb23268a6dbce65f
Gloria_x <656369946@qq.com>
2023-07-18 15:55:21 +0800

    medium

--- type-challenges/easy/easy13_parameters/template.ts
+++ type-challenges/easy/easy13_parameters/template.ts
@@ -1,10 +1,11 @@
 type MyParameters<T extends (...args: any[]) => any> = T extends (
   ...arg: infer P
-) => unknown
+) => any
   ? P
   : never;
 
 /*
 infer -> 推导泛型参数
-因为 (a: string, b: number)=> void extends (a: infer P,b: infer P) => void，所以(a: string, b: number)=> void  是   (a: infer P,b: infer P) => void 子类型
+因为 (a: string, b: number) => void extends (a: infer P,b: infer P) => void，
+所以(a: string, b: number) => void 是 (a: infer P,b: infer P) => void 子类型
 */
